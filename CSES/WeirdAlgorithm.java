e3aba72cbceb74f4a4adbe598b44a4e7664d29a4
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-11-16 01:04:56 +0530

    Done weird algorithm

--- CSES/WeirdAlgorithm.java
+++ CSES/WeirdAlgorithm.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class WeirdAlgorithm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while (n != 1) {
+        long n = sc.nextLong();
+        while (n > 1) {
             System.out.print(n + " ");
             if (n % 2 == 0) {
-                n /= 2;
+                n >>= 1;
             } else {
                 n *= 3;
                 n++;
