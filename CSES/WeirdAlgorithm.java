816bc33ad242d129b87927fb454a9f824a78f9c6
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-11-16 00:30:14 +0530

    First trial commit

---
+++ CSES/WeirdAlgorithm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n != 1) {
+            System.out.print(n + " ");
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n *= 3;
+                n++;
+            }
+        }
+
+        System.out.println(n);
+        sc.close();
+    }
+}
