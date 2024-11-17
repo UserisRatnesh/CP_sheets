3b839514192e1adaab9f08df4d9a527e1e9a00e6
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-11-17 14:22:09 +0530

    Imroved and clean code for Repetitions problem.

--- CSES/Repetitions.java
+++ CSES/Repetitions.java
@@ -6,23 +6,27 @@ import java.util.Scanner;
 public class Repetitions {
 
     public static void main(String[] args) {
+        solveBetter();
+    }
+
+    public static void solveBetter() {
         Scanner sc = new Scanner(System.in);
 
-        int ans = 1;
+        int ans = 0;
         String str = sc.next();
         int n = str.length();
 
-        char prev = str.charAt(0);
-        int count = 1;
-        for (int i = 1; i < n; i++) {
+        char prev = 'S';
+        int count = 0;
+        for (int i = 0; i < n; i++) {
             char c = str.charAt(i);
             if (c == prev) {
                 count++;
             } else {
-                prev = c;
                 count = 1;
+                prev = c;
             }
-            ans = Math.max(count, ans);
+            ans = Math.max(ans, count);
         }
         System.out.println(ans);
         sc.close();
