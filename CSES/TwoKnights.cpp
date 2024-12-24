4079c7f782166c3ac3f20683e4394196f5d8d2e4
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-12-24 23:46:59 +0530

    Two knights done.

---
+++ CSES/TwoKnights.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+  long n;
+  cin >> n;
+  // for each index of grid
+  for (long k = 1; k <= n; ++k) {
+    long ans = (k * k * (k * k - 1) / 2);
+    ans -= 4 * (k - 1) * (k - 2);
+    cout << ans << endl;
+  }
+}
+
+int main() {
+
+  solve();
+
+  return 0;
+}
