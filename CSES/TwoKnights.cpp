080d255f79208d72c548e74accd845757055a7f6
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-12-24 23:54:32 +0530

    Done Two knights and removed executable file.

--- CSES/TwoKnights.cpp
+++ CSES/TwoKnights.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void solve() {
   long n;
   cin >> n;
-  // for each index of grid
+  // for each index of grid k x k
   for (long k = 1; k <= n; ++k) {
     long ans = (k * k * (k * k - 1) / 2);
     ans -= 4 * (k - 1) * (k - 2);
