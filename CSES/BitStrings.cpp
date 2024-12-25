e78a123ddc9c6c623d19d15177c173f6984f65b5
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-12-25 23:25:38 +0530

    Done BitsString.

--- CSES/BitStrings.cpp
+++ CSES/BitStrings.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-
-
 #define pb push_back
-
 using ll = long long;
+#define mod 1000000007
+#define loop(i, start, end) for (ll i = ll(start); i < ll(end); i++)
 
-void solve() {}
+// How to calculate 2 ka power very large
 
-int main() {
-
-  int t;
-  cin >> t;
-
-  while (t--) {
-    solve();
-  }
-
-  return 0;
+ll power_mod(ll n)
+{
+  ll ans = 1;
+  loop(i, 0, n) { ans = (ans * 2) % mod; }
+  return ans;
+}
+int main()
+{
+  ll n;
+  cin >> n;
+  cout << power_mod(n) << endl;
 }
