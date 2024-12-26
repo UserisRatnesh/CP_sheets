466644d0426fc077d0e54bfe3af37fb55b990b06
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-12-27 00:20:00 +0530

    Done 1881A, But kuchh vibe sa nahi aaya.

---
+++ TLE eliminators sheet/Rating800/1881A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+int solve() {
+  ll n, m;
+  cin >> n >> m;
+  string x, s;
+  cin >> x >> s;
+
+  for (int i = 0; i < 5; i++) {
+    if (x.find(s) != -1) {
+      return i;
+    }
+    x.append(x);
+  }
+  return -1;
+}
+
+int main() {
+  ll t;
+  cin >> t;
+  while (t--) {
+    cout << solve() << endl;
+  }
+}
