683f01882399c5b0f96de13664b76b827c4c6987
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-01-22 15:40:43 +0530

    Done maximum subarray sum.

---
+++ codeChef_1600_to_1800_Rated/maximumSubarray.cpp
@@ -0,0 +1,90 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void solve() {
+  ll n, m;
+  cin >> n;
+  vector<ll> a(n);
+
+  ll maxSum = LLONG_MIN;
+  ll sum = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    sum += a[i];
+    maxSum = max(maxSum, sum);
+  }
+
+  sum = 0;
+  for (int i = n - 1; i >= 0; i--) {
+    sum += a[i];
+    maxSum = max(maxSum, sum);
+  }
+
+  ll sumB = 0;
+  cin >> m;
+  for (int i = 0; i < m; i++) {
+    ll val;
+    cin >> val;
+    if (val >= 0) {
+      sumB += val;
+    }
+  }
+
+  cout << maxSum + sumB << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+#endif
+
+  ll t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
