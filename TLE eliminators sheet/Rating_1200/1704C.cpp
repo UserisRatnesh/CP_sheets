301d1a793b37d4d25ae406f5a86134b0dc8206b0
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-04-30 18:37:56 +0530

    Virus.

---
+++ TLE eliminators sheet/Rating_1200/1704C.cpp
@@ -0,0 +1,154 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+void solve() {
+  ll n, m;
+  cin >> n >> m;
+  vector<ll> infected(m);
+  for (auto &it : infected) {
+    cin >> it;
+  }
+
+  sort(all(infected));
+
+  vector<ll> gaps;
+  for (ll i = 1; i < m; i++) {
+    ll gap = infected[i] - infected[i - 1] - 1;
+    gaps.pb(gap);
+  }
+  gaps.pb(n - infected[m - 1] + infected[0] - 1);
+
+  sort(all(gaps));
+  ll len = gaps.size();
+
+  ll steps = 0;
+  ll notInf = 0;
+
+  for (ll i = len - 1; i >= 0; i--) {
+    ll leftGap = gaps[i] - steps * 2;
+    if (leftGap == 1) {
+      steps++;
+      notInf += 1;
+    } else if (leftGap == 2) {
+      steps++;
+      notInf += 1;
+    } else if (leftGap > 0) {
+      steps += 2;
+      notInf += leftGap - 1;
+    }
+  }
+
+  cout << n - notInf << nl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  // freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  fastio();
+  ll t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
